ba5835f4895ea0132b61553ba8bdf714327a15d6
Severin <severin.hitzler@bshg.com>
2023-01-09 18:11:24 +0100

    refactor: Display of station stats

--- bikeshare.py
+++ bikeshare.py
@@ -119,13 +119,13 @@ def station_stats(df):
     start_time = time.time()
 
     # display most commonly used start station
-    print('The most common start station is ' + df['Start Station'].mode()[0] + '.')
+    print('The most common start station is {}.'.format(df['Start Station'].mode()[0]))
 
     # display most commonly used end station
-    print('The most common end station is ' + df['End Station'].mode()[0] + '.')
+    print('The most common end station is {}.'.format(df['End Station'].mode()[0]))
 
     # display most frequent combination of start station and end station trip
-    print('The frequent combination of start station and end station is ' + df['Start End'].mode()[0] + '.')
+    print('The frequent combination of start station and end station is {}.'.format(df['Start End'].mode()[0]))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
